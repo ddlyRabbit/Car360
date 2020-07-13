6e32fd00629a908ae1bf961e50fc64d23e7652cd
Abhinav Roy <abhinavroy@pop-os.localdomain>
2020-07-13 20:29:23 +0530

    made png stitch transparent

--- pillow_stitch.py
+++ pillow_stitch.py
@@ -13,11 +13,13 @@ image = Image.open(images[0])
 width, height = image.size
 result_width = 1800 * len(images)
 result_height = 1200
-result_image = Image.new('RGB',(result_width,result_height),(255, 255, 255))
+result_image = Image.new('RGBA',(result_width,result_height),(255, 255, 255))
+result_image.save(folder + "/canvas_image.png")
 
 for i,image_path in enumerate(images):
     image = Image.open(image_path)
     image = image.resize((1800,1200))
-    result_image.paste(im=image, box=(i*1800,0))
+    print(image.mode)
+    result_image.paste(im=image, box=(i*1800,0), mask=image)
 
 result_image.save(folder + "/stitched_image.png")
