2d285fce469cc01025ccf53bc312beef64193a84
Abhinav Roy <abhinavroy@pop-os.localdomain>
2020-07-13 19:59:51 +0530

    freestyle added

--- pillow_stitch.py
+++ pillow_stitch.py
@@ -7,17 +7,17 @@ import time
 import copy
 
 folder = "360photos_comp"
-images = glob.glob(folder + "/*.jpg")
+images = glob.glob(folder + "/*.png")
 images = sorted(images)
 image = Image.open(images[0])
 width, height = image.size
 result_width = 1800 * len(images)
 result_height = 1200
-result_image = Image.new('RGB',(result_width,result_height))
+result_image = Image.new('RGB',(result_width,result_height),(255, 255, 255))
 
 for i,image_path in enumerate(images):
     image = Image.open(image_path)
     image = image.resize((1800,1200))
     result_image.paste(im=image, box=(i*1800,0))
 
-result_image.save(folder + "/stitched_image.jpg")
+result_image.save(folder + "/stitched_image.png")
